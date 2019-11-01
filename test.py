e3c9e4b79bdb86ea8b817adb251f4105a286401a
Stanislaw Pawlak <stanislawpawlak3@gmail.com>
2019-11-01 16:45:36 +0100

    add: tests for local and global alignment.

--- test.py
+++ test.py
@@ -1,8 +1,25 @@
 import pytest
 from main import *
 
+
 def test_possible_moves():
     assert possible_moves(0, 0) == []
-    assert possible_moves(1, 0) == [(-1, 0, UP)]
-    assert possible_moves(0, 2) == [(0, -1, LEFT)]
-    assert possible_moves(1, 1) == [(0, -1, LEFT), (-1, -1, UP_LEFT), (-1, 0, UP)]
+    assert possible_moves(1, 0) == [UP]
+    assert possible_moves(0, 2) == [LEFT]
+    assert possible_moves(1, 1) == [LEFT, UP, UP_LEFT]
+
+
+def test_global():
+    s1 = "CAAGAC"
+    s2 = "GAAC"
+    line, j = calculate_alignment(s1, s2)
+    assert line == "*AA--C"
+    assert j == 0
+
+
+def test_local():
+    s1 = "CAAGAC"
+    s2 = "AGA"
+    line, j = calculate_alignment(s1, s2, LOCAL)
+    assert line == "AGA"
+    assert j == 2
